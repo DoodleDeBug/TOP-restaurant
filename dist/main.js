524c57e57d138a65c63f503109f2451f5acdfeec
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-09-06 16:51:36 +0100

    plan intial menu

--- dist/main.js
+++ dist/main.js
@@ -7,15 +7,16 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/contact.js":
 /*!************************!*\
   !*** ./src/contact.js ***!
   \************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://top-restaurant/./src/contact.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction contact() {\n  const content = document.getElementById(\"content\");\n\n  const section = document.createElement(\"div\");\n  section.classList.add(\"section\");\n\n  const title = document.createElement(\"h2\");\n  title.innerText = \"Contact\";\n\n  section.appendChild(title);\n\n  content.appendChild(section);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (contact);\n\n\n//# sourceURL=webpack://top-restaurant/./src/contact.js?");
 
 /***/ }),
 
@@ -23,9 +24,9 @@ eval("\n\n//# sourceURL=webpack://top-restaurant/./src/contact.js?");
 /*!************************!*\
   !*** ./src/gallery.js ***!
   \************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://top-restaurant/./src/gallery.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction gallery() {\n  const content = document.getElementById(\"content\");\n\n  const section = document.createElement(\"div\");\n  section.classList.add(\"section\");\n\n  const title = document.createElement(\"h2\");\n  title.innerText = \"Gallery\";\n\n  section.appendChild(title);\n\n  content.appendChild(section);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (gallery);\n\n\n//# sourceURL=webpack://top-restaurant/./src/gallery.js?");
 
 /***/ }),
 
@@ -35,7 +36,6 @@ eval("\n\n//# sourceURL=webpack://top-restaurant/./src/gallery.js?");
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction home() {\n  const content = document.getElementById(\"content\");\n\n  const section = document.createElement(\"div\");\n  section.classList.add(\"section\");\n\n  const title = document.createElement(\"h2\");\n  title.innerText = \"About Us\";\n\n  const p = document.createElement(\"p\");\n  p.innerText =\n    \"We are a resaurant specialising in seafood. We love fish. Come to our resaurant, it's very cool.\";\n\n  section.appendChild(title);\n  section.appendChild(p);\n\n  content.appendChild(section);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (home);\n\n\n//# sourceURL=webpack://top-restaurant/./src/home.js?");
 
 /***/ }),
@@ -46,8 +46,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pageLoad__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./pageLoad */ \"./src/pageLoad.js\");\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n/* harmony import */ var _gallery__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./gallery */ \"./src/gallery.js\");\n/* harmony import */ var _gallery__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_gallery__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_menu__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./contact */ \"./src/contact.js\");\n/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_contact__WEBPACK_IMPORTED_MODULE_4__);\n\n\n\n\n\n\n(0,_pageLoad__WEBPACK_IMPORTED_MODULE_0__.default)();\n\n//navbar activation\n(function () {\n  const homebtn = document.getElementById(\"home\");\n  const menubtn = document.getElementById(\"menu\");\n  const gallerybtn = document.getElementById(\"gallery\");\n  const contactbtn = document.getElementById(\"contact\");\n\n  let btns = [homebtn, menubtn, gallerybtn, contactbtn];\n\n  btns.forEach((btn) => {\n    let name = btn.id;\n    btn.addEventListener(\"click\", () => {\n      console.log(`clicked btn ${name}`);\n      switch (name) {\n        case \"home\":\n          (0,_home__WEBPACK_IMPORTED_MODULE_1__.default)();\n          break;\n        case \"menu\":\n          _menu__WEBPACK_IMPORTED_MODULE_3___default()();\n          break;\n        case \"gallery\":\n          _gallery__WEBPACK_IMPORTED_MODULE_2___default()();\n          break;\n        case \"contact\":\n          _contact__WEBPACK_IMPORTED_MODULE_4___default()();\n          break;\n        default:\n          console.log(\"not work\");\n      }\n    });\n  });\n})();\n\n\n//# sourceURL=webpack://top-restaurant/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pageLoad__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./pageLoad */ \"./src/pageLoad.js\");\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n/* harmony import */ var _gallery__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./gallery */ \"./src/gallery.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./contact */ \"./src/contact.js\");\n\n\n\n\n\n\n(0,_pageLoad__WEBPACK_IMPORTED_MODULE_0__.default)();\n\n//navbar activation\n(function () {\n  const homebtn = document.getElementById(\"home\");\n  const menubtn = document.getElementById(\"menu\");\n  const gallerybtn = document.getElementById(\"gallery\");\n  const contactbtn = document.getElementById(\"contact\");\n\n  let btns = [homebtn, menubtn, gallerybtn, contactbtn];\n\n  btns.forEach((btn) => {\n    let name = btn.id;\n    btn.addEventListener(\"click\", () => {\n      console.log(`clicked btn ${name}`);\n      switch (name) {\n        case \"home\":\n          (0,_home__WEBPACK_IMPORTED_MODULE_1__.default)();\n          break;\n        case \"menu\":\n          (0,_menu__WEBPACK_IMPORTED_MODULE_3__.default)();\n          break;\n        case \"gallery\":\n          (0,_gallery__WEBPACK_IMPORTED_MODULE_2__.default)();\n          break;\n        case \"contact\":\n          (0,_contact__WEBPACK_IMPORTED_MODULE_4__.default)();\n          break;\n        default:\n          console.log(\"not work\");\n      }\n    });\n  });\n})();\n\n\n//# sourceURL=webpack://top-restaurant/./src/index.js?");
 
 /***/ }),
 
@@ -55,9 +54,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pag
 /*!*********************!*\
   !*** ./src/menu.js ***!
   \*********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://top-restaurant/./src/menu.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction menu() {\n  const content = document.getElementById(\"content\");\n\n  const section = document.createElement(\"div\");\n  section.classList.add(\"section\");\n\n  const title = document.createElement(\"h2\");\n  title.innerText = \"Menu\";\n\n  const menuSection = document.createElement(\"div\");\n  menuSection.classList.add(\"menu-section\");\n\n  const menuSectionTitle = document.createElement(\"h3\");\n  menuSectionTitle.innerText = \"Mains\";\n\n  const menuSectionContent = document.createElement(\"div\");\n  menuSectionContent.classList.add(\"menu-section-content\");\n  menuSectionContent.innerText = \"menu content\";\n\n  menuSection.appendChild(menuSectionTitle);\n  menuSection.appendChild(menuSectionContent);\n\n  section.appendChild(title);\n  section.appendChild(menuSection);\n\n  content.appendChild(section);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (menu);\n\n\n//# sourceURL=webpack://top-restaurant/./src/menu.js?");
 
 /***/ }),
 
@@ -67,7 +66,6 @@ eval("\n\n//# sourceURL=webpack://top-restaurant/./src/menu.js?");
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction pageLoad() {\n  const content = document.getElementById(\"content\");\n\n  const headline = document.createElement(\"h1\");\n  headline.innerText = \"My Awesome Restaurant\";\n\n  const copy = document.createElement(\"p\");\n  copy.innerText = \"Fresh, Delicious, Awesome Food\";\n\n  const container = document.createElement(\"div\");\n  container.classList.add(\"container\");\n  container.appendChild(headline);\n  container.appendChild(copy);\n\n  const nav = document.createElement(\"nav\");\n  nav.classList.add(\"navbar\");\n\n  const ul = document.createElement(\"ul\");\n  nav.appendChild(ul);\n\n  let navbtns = [\"Home\", \"Menu\", \"Gallery\", \"Contact\"];\n\n  navbtns.forEach((btn) => {\n    const li = document.createElement(\"li\");\n    li.innerText = btn;\n    li.id = btn.toLowerCase();\n    ul.appendChild(li);\n  });\n\n  content.appendChild(container);\n  content.appendChild(nav);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (pageLoad);\n\n\n//# sourceURL=webpack://top-restaurant/./src/pageLoad.js?");
 
 /***/ })
@@ -99,18 +97,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
--- src/menu.js
+++ src/menu.js
@@ -7,7 +7,21 @@ function menu() {
   const title = document.createElement("h2");
   title.innerText = "Menu";
 
+  const menuSection = document.createElement("div");
+  menuSection.classList.add("menu-section");
+
+  const menuSectionTitle = document.createElement("h3");
+  menuSectionTitle.innerText = "Mains";
+
+  const menuSectionContent = document.createElement("div");
+  menuSectionContent.classList.add("menu-section-content");
+  menuSectionContent.innerText = "menu content";
+
+  menuSection.appendChild(menuSectionTitle);
+  menuSection.appendChild(menuSectionContent);
+
   section.appendChild(title);
+  section.appendChild(menuSection);
 
   content.appendChild(section);
 }
