524f08053f6f5d5cf19fd0760491ef7369d0bf17
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-09-17 20:48:40 +0100

    import css properly

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import home from "./home";
 import gallery from "./gallery";
 import menu from "./menu";
 import contact from "./contact";
-import css from "./style.css";
+import "./style.css";
 
 pageLoad();
 
